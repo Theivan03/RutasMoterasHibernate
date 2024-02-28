5c47dc3c5894cc4d610b3f5ad94211d5b885b314
Theivan03 <93394038+Theivan03@users.noreply.github.com>
2024-02-28 20:07:03 +0100

    Primer commit con proyecto a 0

---
+++ RutasMoteras/src/test/java/com/example/RutasMoteras/RutasMoterasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.RutasMoteras;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RutasMoterasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
